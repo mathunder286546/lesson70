a7377265d2247dba318ab3536cfbf87ac0478b2f
valentinaivanova1910 <125904818+valentinaivanova1910@users.noreply.github.com>
2023-06-01 18:20:42 +0300

    Add project files.

---
+++ lesson70/main.cpp
@@ -0,0 +1,19 @@
+#include"main.h"
+
+class calculator {
+public:
+	int add(int a, int b) {
+		return a + b;
+
+	}
+	static int add (int a, int b, int c) {
+		return a + b + c;
+	}
+	static double add(double a, double b) {
+		return a + b;
+	}
+};
+
+int main() {
+	Calculator
+}
